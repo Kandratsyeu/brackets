b99af83284b059559bf34a4807bf4369802cb8d9
Siarhei Kandratsyeu <sukandratsyeu@gmail.com>
2021-03-09 15:26:40 +0300

    task is completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let stackOfBrackets = [str[0]];
+    for (let i = 1; i < str.length; i++) {
+        let current = str[i];
+        function getPair(element, index, array) {
+            return element[1] == str[i];
+        }
+        let pair = bracketsConfig.find(getPair);
+        if (!pair) {
+            stackOfBrackets.push(current);
+        } else if (
+            pair &&
+            stackOfBrackets[stackOfBrackets.length - 1] == pair[0]
+        ) {
+            stackOfBrackets.pop();
+        } else {
+            stackOfBrackets.push(current);
+        }
+    }
+    return stackOfBrackets.length == 0;
+};
